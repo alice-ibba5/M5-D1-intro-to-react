9e8d5a26c17999984ed0f6df37682daabcd44623
alice-ibba5 <135819129+alice-ibba5@users.noreply.github.com>
2023-10-18 13:22:46 +0200

    UPDATE

---
+++ index.js
@@ -0,0 +1,39 @@
+/*
+
+Fornisci una risposta alle seguenti domande.
+
+1. Cos’è ReactJS? (risposta aperta)
+React è una libreria JavaScript per la creazione di interfacce utente (UI, User Interface).
+React consente di sviluppare applicazioni dinamiche che non necessitano di ricaricare la pagina per visualizzare i dati modificati. 
+Inoltre nelle applicazioni React le modifiche effettuate sul codice si possono visualizzare in tempo reale, permettendo uno sviluppo rapido, 
+efficiente e flessibile delle applicazioni web.
+
+2. ReactJS è un framework, non una libreria. FALSO
+
+3. Il file package.json contiene…
+- Molte informazioni utili, come ad esempio l’elenco di tutte le dipendenze richieste dall’applicazione 
+
+4. create-react-app è l’unico modo possibile per creare un’applicazione React FALSO
+
+5. Qual è il comando da lanciare nel terminale per creare una nuova create-react-app con nome “test”?
+- npx create-react-app test 
+
+6. Cos’è un componente React?
+- Un blocco di logica/contenuto riutilizzabile all’interno dell’applicazione
+
+7. Un componente React può venire creato in tre modi: come funzione, classe o interfaccia. FALSO
+
+8. Qual è la differenza tra componenti creati come funzione e componenti creati come classe?
+- I componenti creati come funzione sono più semplici da scrivere ma con meno funzionalità, mentre i componenti creati come classe necessitano in genere di un maggior numero di linee di codice ma possiedono più funzionalità.
+
+9. Le props sono frammenti di informazione assegnati all’invocazione di un componente React, 
+utili al fine di rendere il componente dinamico e più riutilizzabile. VERO
+
+10. Le props possono essere passate solamente da un componente genitore ad un componente figlio, non è possibile fare il contrario VERO
+
+11. Da dove possono venire recuperate le props all’interno di un componente React creato come classe?
+
+- Possono essere recuperate all’interno dello oggetto ‘this’, dentro un sotto-oggetto chiamato ‘props’
+
+
+*/
